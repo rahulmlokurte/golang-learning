bdfec3763a6bd7d48425a3884ab3c4082eb6e4ab
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2023-12-26 17:52:35 +0530

    Add interface

---
+++ interface/go.mod
@@ -0,0 +1,3 @@
+module github.com/secure-super/interface
+
+go 1.21.5
---
+++ interface/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+  "fmt"
+)
+
+type foobar interface {
+  foo()
+  bar()
+}
+
+type itemA struct{}
+
+func (a *itemA) foo() {
+  fmt.Println("foo on A")
+}
+
+func (a *itemA) bar() {
+  fmt.Println("foo on C")
+}
+
+type itemB struct{}
+
+func (a *itemB) foo() {
+  fmt.Println("foo on B")
+}
+
+func (b *itemB) bar() {
+  fmt.Println("foo on B")
+}
+
+func doFoo(item foobar) {
+  item.foo()
+  item.bar()
+}
+
+func main() {
+  doFoo(&itemA{})
+  doFoo(&itemB{})
+}
+
+
+
